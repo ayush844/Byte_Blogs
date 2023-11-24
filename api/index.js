0216c5bbf5e475d093b4da2da270658b76ba7164
ayush <ayush.xyz1625@gmail.com>
2023-11-24 20:59:07 +0530

    connected to databse

--- api/index.js
+++ api/index.js
@@ -1,6 +1,15 @@
 import express from 'express'
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
 
 
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log("connected to mongoDB successfully :)");
+}).catch((err)=>{
+    console.log(err);
+})
 
 const app = express();
 
