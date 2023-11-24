bdd8cffc04e188d8ebeaa85c35df26f1367672e2
ayush <ayush.xyz1625@gmail.com>
2023-11-24 10:48:01 +0530

    installed react js

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import './App.css'
+
+const App = () => {
+  return (
+    <div>
+      APP
+    </div>
+  )
+}
+
+export default App
