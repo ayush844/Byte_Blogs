abc2ab25d18c1cb7fbef76a275e8a442ff4d0db1
ayush <ayush.xyz1625@gmail.com>
2023-11-28 01:53:53 +0530

    worked on profile page UI

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -16,7 +16,7 @@ const userSchema = new mongoose.Schema({
     },
     avatar:{
         type: String,
-        default: "https://w7.pngwing.com/pngs/340/946/png-transparent-avatar-user-computer-icons-software-developer-avatar-child-face-heroes-thumbnail.png"
+        default: "../avatar_pic.png"
     }
 }, {timestamps: true});
 
--- client/src/Pages/Profile.jsx
+++ client/src/Pages/Profile.jsx
@@ -1,11 +1,54 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
 import './Profile.css'
+import { useSelector } from 'react-redux'
+import { MdDelete } from "react-icons/md";
+import { FaSignOutAlt } from "react-icons/fa";
 
 const Profile = () => {
+
+  const {currentUser} = useSelector((state)=>state.user);
+
+
   return (
-    <div>
-      <h1 style={{fontSize:'5rem'}}>profile</h1>
+    <div className='profilePage'>
+
+      <div className="userInfo">
+        <img src={currentUser.avatar} alt="profile-pic" />
+        <h2>{currentUser.username}</h2>
+      </div>
+
+      <div className="follows">
+        <div className="following">
+          <h4>FOLLOWING</h4>
+          <h3>132</h3>
+        </div>
+        <div className="following">
+          <h4>FOLLOWERS</h4>
+          <h3>546</h3>
+        </div>
+      </div>
+
+      <div className="articles">
+        <h4>articles published: <span>6</span></h4>
+      </div>
+
+      <button>UPDATE PROFILE</button>
+
+      <div className="others">
+        <div className="otherCompnents">
+          <MdDelete style={{fontSize:'2.5rem', color:'red', cursor:'pointer'}}/>
+          <span>DELETE ACCOUNT</span>
+        </div>
+        <div className="otherCompnents">
+          <FaSignOutAlt style={{fontSize:'2.5rem', color:'red', cursor:'pointer'}}/>
+          <span>SIGN OUT</span>
+        </div>
+        
+      </div>
+
+
+
     </div>
   )
 }
