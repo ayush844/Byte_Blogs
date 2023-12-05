b43e9ac36593af60b812e5ae0a3844728862cdef
ayush844 <ayush.xyz1625@gmail.com>
2023-12-06 02:28:47 +0530

    corrected firebase for file upload

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,22 +1,17 @@
-/* eslint-disable no-unused-vars */
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "byte-blogs.firebaseapp.com",
-  projectId: "byte-blogs",
-  storageBucket: "byte-blogs.appspot.com",
-  messagingSenderId: "871846857024",
-  appId: "1:871846857024:web:0105d422c19c792db8d2a5",
-  measurementId: "G-GE87N42HYL"
+  authDomain: "byteblog-8dfcd.firebaseapp.com",
+  projectId: "byteblog-8dfcd",
+  storageBucket: "byteblog-8dfcd.appspot.com",
+  messagingSenderId: "1063691136208",
+  appId: "1:1063691136208:web:7abdd644d78023f6cb42d5"
 };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
