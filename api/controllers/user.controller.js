58a1d50f9fb0b429a7976171c19b345e6d42ecc2
ayush844 <ayush.xyz1625@gmail.com>
2023-12-13 00:34:40 +0530

    improved the delete user functionality

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -41,6 +41,19 @@ export const deleteUser = async(req, res, next)=>{
 
     try {
 
+        const userToDelete = await User.findById(req.params.id);
+        const followers = userToDelete.followers;
+        const following = userToDelete.following;
+
+        for (const followerId of followers) {
+            await User.findByIdAndUpdate(followerId, { $pull: { following: req.params.id } });
+        }
+
+        for (const followingId of following) {
+            await User.findByIdAndUpdate(followingId, { $pull: { followers: req.params.id } });
+        }
+        
+
         await User.findByIdAndDelete(req.params.id);
         res.clearCookie('access_token');
         res.status(200).json("User has been deleted successfully");
