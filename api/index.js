31a91f9aeaf27abbae25a1202072c0dacff8e479
ayush844 <ayush.xyz1625@gmail.com>
2024-01-29 14:07:04 +0530

    add pre installation

--- api/index.js
+++ api/index.js
@@ -1,20 +1,26 @@
-import express from 'express'
-import mongoose from 'mongoose';
-import dotenv from 'dotenv'
-import userRouter from './routes/user.route.js'
-import authRouter from './routes/auth.route.js'
-import blogRouter from './routes/blog.route.js'
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
+import blogRouter from "./routes/blog.route.js";
 
-import cookieParser from 'cookie-parser';
+import cookieParser from "cookie-parser";
 
+import path from "path";
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO).then(()=>{
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
     console.log("connected to mongoDB successfully :)");
-}).catch((err)=>{
+  })
+  .catch((err) => {
     console.log(err);
-})
+  });
+
+const __dirname = path.resolve();
 
 const app = express();
 
@@ -22,29 +28,28 @@ app.use(express.json());
 
 app.use(cookieParser());
 
-
-app.listen(3000, ()=>{
-    console.log("server is running on port 3000!!!");
-})
-
-
+app.listen(3000, () => {
+  console.log("server is running on port 3000!!!");
+});
 
 app.use("/api/user", userRouter);
 
-
 app.use("/api/auth", authRouter);
-
 
 app.use("/api/blog", blogRouter);
 
+app.use(express.static(path.join(__dirname, "/client/dist")));
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.use((err, req, res, next) => {
-    const statusCode = err.statusCode || 500;
-    const message = err.message || "internal server error";
-    return res.status(statusCode).json({
-        success: false,
-        statusCode,
-        message
-    });
-})
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "internal server error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
