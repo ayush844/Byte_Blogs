19be377ed794a40cb89adb0a97a0f66c856a3b8f
ayush844 <ayush.xyz1625@gmail.com>
2024-01-29 12:39:03 +0530

    made error page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,48 +1,49 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
-import './App.css'
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import Home from './Pages/Home';
-import About from './Pages/About';
-import SignIn from './Pages/SignIn';
-import SignUp from './Pages/SignUp';
-import Profile from './Pages/Profile';
-import Sidebar from './Components/Sidebar';
-import Explore from './Pages/Explore';
-import Bookmarks from './Pages/Bookmarks';
-import Following from './Pages/Following';
-import PrivateRoute from './Components/PrivateRoute';
-import UpdateProfile from './Pages/UpdateProfile';
-import CreateBlog from './Pages/CreateBlog';
-import Author from './Pages/Author';
-import BlogPage from './Pages/BlogPage';
-import UpdateBlog from './Pages/UpdateBlog';
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import SignIn from "./Pages/SignIn";
+import SignUp from "./Pages/SignUp";
+import Profile from "./Pages/Profile";
+import Sidebar from "./Components/Sidebar";
+import Explore from "./Pages/Explore";
+import Bookmarks from "./Pages/Bookmarks";
+import Following from "./Pages/Following";
+import PrivateRoute from "./Components/PrivateRoute";
+import UpdateProfile from "./Pages/UpdateProfile";
+import CreateBlog from "./Pages/CreateBlog";
+import Author from "./Pages/Author";
+import BlogPage from "./Pages/BlogPage";
+import UpdateBlog from "./Pages/UpdateBlog";
+import Error from "./Pages/Error";
 
 const App = () => {
   return (
     <BrowserRouter>
-    <Sidebar>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/sign-in' element={<SignIn/>} />
-        <Route path='/sign-up' element={<SignUp />} />
-        <Route path='/author/:id' element={<Author />} />
-        <Route path='/blog/:id' element={<BlogPage />} />
-        <Route element={<PrivateRoute />}>
-          <Route path='/profile' element={<Profile />} />
-          <Route path='/updateProfile' element={<UpdateProfile />} />
-          <Route path='/createBlog' element={<CreateBlog />} />
-          <Route path='/updateBlog/:id' element={<UpdateBlog />} />
-          <Route path='/bookmarks' element={<Bookmarks />} />
-          <Route path='/following' element={<Following />} />
-        </Route>
-        <Route path='/search' element={<Explore />} />
-
-      </Routes>
-    </Sidebar>
+      <Sidebar>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/author/:id" element={<Author />} />
+          <Route path="/blog/:id" element={<BlogPage />} />
+          <Route element={<PrivateRoute />}>
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/updateProfile" element={<UpdateProfile />} />
+            <Route path="/createBlog" element={<CreateBlog />} />
+            <Route path="/updateBlog/:id" element={<UpdateBlog />} />
+            <Route path="/bookmarks" element={<Bookmarks />} />
+            <Route path="/following" element={<Following />} />
+          </Route>
+          <Route path="/search" element={<Explore />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </Sidebar>
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ client/src/Pages/Error.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import "./Error.css";
+import styled from "styled-components";
+import error from "../assets/error.png";
+
+const Wrapper = styled.section`
+  padding: 9rem 2rem;
+  display: flex;
+  gap: 3rem;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+
+  h2 {
+    font-size: 3rem;
+    color: #ed5ab3;
+  }
+
+  @media (max-width: 600px) {
+    h2 {
+      font-size: 2rem;
+    }
+
+    img {
+      width: 25rem;
+    }
+  }
+`;
+
+const Error = () => {
+  return (
+    <Wrapper style={{ color: "white" }}>
+      <img src={error} alt="error" />
+      <h2>PAGE NOT FOUND</h2>
+    </Wrapper>
+  );
+};
+
+export default Error;
